749bc5a12ebd06f1c48b090477378d38bacb3ec5
Szendrényi Ákos <Szendrnyi kos@DESKTOP-SG6VCUU>
2018-11-01 14:38:12 +0100

    Kész

--- src/feladat_3/Feladat_3.java
+++ src/feladat_3/Feladat_3.java
@@ -11,12 +11,25 @@ public class Feladat_3 {
         
         
         
-        double szam;
+        double szam ;
         Scanner input = new Scanner(System.in);
        
         
         System.out.println("Kérem adja meg a számot");
          szam= input.nextDouble();
+        
+        
+        int decimal = (int) szam; 
+        double fractional = szam - decimal ;
+        
+        double a = decimal;
+        double b = decimal+1;
+        double c = szam;
+        double closer = Math.abs(c - a) < Math.abs(c - b) ? a : b;
+          System.out.println("A megadott szám a "+(decimal)+" és a "+(decimal+1)+" egész számok között van,és ezek közül a " +closer+" számhoz van közelebb.");
+          System.out.println("A szám egész része: "+decimal);
+          System.out.println("A szám törtrésze: "+fractional);
+          
     }
     
 }
